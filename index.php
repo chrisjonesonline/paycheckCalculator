3f62c8063f9cf580b6da8e3d9015d83a7722662c
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2022-07-12 16:45:00 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-<h1>Weekly Paycheck Calculator</h1>
+<h1>Paycheck Calculator</h1>
   <hr />
   <form class="form" method="POST">
     <p>Please Enter Salary:
