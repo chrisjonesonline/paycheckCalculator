d43850019e7151aeebc0d7d37357cdbb1980518f
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2022-07-12 16:37:14 -0700

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,26 @@
 		</p>
 		<input type="submit" name="submit" value="Submit"></input>
 	</form>
-	<p><?php include('assets/php/controllers/calculate.php'); ?></p>
+<?php
+  //disable error reporting
+  error_reporting(E_ERROR|E_PARSE);
+  
+  if(isset($_POST['submit'])) {
+  	$salary = ($_POST['salary']);
+  	$hours = ($_POST['hours']);
+  	  if ($hours <= 40) {
+        $paycheck = $salary * $hours;
+        $overtime = 0;
+  	  }
+      else {
+        $paycheck = $salary * 40;
+  		  $overtime = ((($hours - 40) * $salary) * 1.5);
+      }
+      $total = $paycheck + $overtime;
+  	echo '<br />' . 'Salary: ' . '$' . $paycheck;
+  	echo '<br />' . 'Overtime: ' . '$' . $overtime;
+    echo '<br />' . 'Total: ' . '$' . $total;
+  }
+?>
 </body>
 </html>
