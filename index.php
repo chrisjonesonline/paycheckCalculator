36d1be919db9cc9a9458f6217ad4493974db470d
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2022-07-12 17:43:34 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
 <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-<h1>Paycheck Calculator</h1>
-  <hr />
+<h1>Paycheck Calculator</h1><hr />
   <form class="form" method="POST">
     <p>Please Enter Salary:
       <input type="int" name="salary" autocomplete="off" autofocus="autofocus" onfocus="this.select();"></input>
